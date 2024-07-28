fa403512988d2611fd0d3ad39af33611ffe7d389
33 <1554470705@qq.com>
2024-07-28 14:55:49 +0800

    POI-excel导出功能

--- sky-server/src/main/java/com/sky/controller/admin/ReportController.java
+++ sky-server/src/main/java/com/sky/controller/admin/ReportController.java
@@ -15,6 +15,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletResponse;
 import java.time.LocalDate;
 
 /**
@@ -64,6 +65,13 @@ public class ReportController {
         log.info("top10:{},{}", begin, end);
         SalesTop10ReportVO salesTop10ReportVO = reportService.getSalesTop10(begin, end);
         return Result.success(salesTop10ReportVO);
+    }
+
+    @GetMapping("/export")
+    @ApiOperation("excel导出运营数据报表")
+    public void export(HttpServletResponse response){
+        log.info("excel导出运营数据报表");
+        reportService.exportBusinessData(response);
     }
 
 }
